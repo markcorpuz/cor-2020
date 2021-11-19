5cb1b19f55f1a5c9419639ff54c11607bfef91a8
Mark Corpuz <social@markcorpuz.com>
2021-11-18 17:08:21 -0800

    COR 1.0.10 | General CSS cleanup

---
+++ assets/js/editor-min.js
@@ -0,0 +1,1 @@
+wp.domReady((()=>{wp.blocks.unregisterBlockStyle("core/button",["default","outline","squared","fill"]),wp.blocks.registerBlockStyle("core/button",[{name:"default",label:"Default",isDefault:!0},{name:"full",label:"Full Width"}]),wp.blocks.unregisterBlockStyle("core/separator",["default","wide","dots"]),wp.blocks.unregisterBlockStyle("core/quote",["default","large"])}));
